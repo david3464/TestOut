b22861ab9be1b78c5303e0196580a1e2ea8e3872
David Chen <davidchen3501@gmail.com>
2019-09-03 04:06:36 +0800

    1

--- 1.On Progress/1.1PA-SlideRotate/public/javascripts/jsfunctionalprogramming.js
+++ 1.On Progress/1.1PA-SlideRotate/public/javascripts/jsfunctionalprogramming.js
@@ -55,15 +55,3 @@ var totalamount2 = orders.reduce((sum,order)=>{
 },0)
 console.log(totalamount2)
 
-{
-    'mark johansson': [
-        {name: 'waffle iron', price:'80', quantity:'2' },
-        {name: 'blender', price:'200', quantity:'1' },
-        {name: 'knife', price:'10', quantity:'4' },
-    ],
-    'Nikita Smith': [
-        {name: 'waffle iron', price:'80', quantity:'2' },
-        {name: 'pot', price:'20', quantity:'3' },
-        {name: 'knife', price:'10', quantity:'2' },
-    ],
-}
