5db62937fe646d959b3e76f1dbefae459d47cf78
David Chen <davidchen3501@gmail.com>
2019-09-03 06:19:47 +0800

    categories course page

--- Test LMS/routes/courses.js
+++ Test LMS/routes/courses.js
@@ -13,7 +13,56 @@ const Class = require('../models/Course');
 router.get('/', function(req, res, next) {
 	Class.getClasses(function(err, classes){
 		res.render('courses/course_index', { classes });
-	},3);
+	});
+});
+
+//router address localhost:3000/courses/featurecourse
+//descriptions: Feature Course Page
+//comments: 
+router.get('/featurecourse', function(req, res, next) {
+		res.render('courses/1featurecourse');
+});
+
+//router address localhost:3000/courses/commoncourse
+//descriptions: Common Course Page
+//comments: 
+router.get('/commoncourse', function(req, res, next) {
+    res.render('courses/2commoncourse');
+});
+
+//router address localhost:3000/courses/fronthostcourse
+//descriptions: Front Desk Host Course Page
+//comments: 
+router.get('/fronthostcourse', function(req, res, next) {
+    res.render('courses/3fronthostcourse');
+});
+
+//router address localhost:3000/courses/servercourse
+//descriptions: Front Desk Host Course Page
+//comments: 
+router.get('/servercourse', function(req, res, next) {
+    res.render('courses/4servercourse');
+});
+
+//router address localhost:3000/courses/sushibarrollercourse
+//descriptions: Front Desk Host Course Page
+//comments: 
+router.get('/sushibarrollercourse', function(req, res, next) {
+    res.render('courses/5sushibarrollercourse');
+});
+
+//router address localhost:3000/courses/kitchenchefcourse
+//descriptions: Front Desk Host Course Page
+//comments: 
+router.get('/kitchenchefcourse', function(req, res, next) {
+    res.render('courses/6kitchenchefcourse');
+});
+
+//router address localhost:3000/courses/managercourse
+//descriptions: Front Desk Host Course Page
+//comments: 
+router.get('/managercourse', function(req, res, next) {
+    res.render('courses/7managercourse');
 });
 
 //router address localhost:3000/courses/details
