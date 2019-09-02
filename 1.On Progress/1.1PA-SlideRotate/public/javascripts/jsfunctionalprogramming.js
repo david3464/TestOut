f70f465b7004b79e44cb37b83bb73f746255676f
David Chen <davidchen3501@gmail.com>
2019-09-03 04:03:46 +0800

    1

--- 1.On Progress/1.1PA-SlideRotate/public/javascripts/jsfunctionalprogramming.js
+++ 1.On Progress/1.1PA-SlideRotate/public/javascripts/jsfunctionalprogramming.js
@@ -51,8 +51,19 @@ console.log(totalamount1)
 
 var totalamount2 = orders.reduce((sum,order)=>{
     console.log("hello",sum, order)
-    return sum+order.amount},0)
+    return sum+order.amount;
+},0)
 console.log(totalamount2)
 
-
-console.log('test')
+{
+    'mark johansson': [
+        {name: 'waffle iron', price:'80', quantity:'2' },
+        {name: 'blender', price:'200', quantity:'1' },
+        {name: 'knife', price:'10', quantity:'4' },
+    ],
+    'Nikita Smith': [
+        {name: 'waffle iron', price:'80', quantity:'2' },
+        {name: 'pot', price:'20', quantity:'3' },
+        {name: 'knife', price:'10', quantity:'2' },
+    ],
+}
