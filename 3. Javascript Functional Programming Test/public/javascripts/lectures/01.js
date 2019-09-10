4008294006b24d2976d93d691ab2865dbbeb3596
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-10 14:29:32 -0400

    1

---
+++ 3. Javascript Functional Programming Test/public/javascripts/lectures/01.js
@@ -0,0 +1,10 @@
+function toGreen() {
+    getElementById('div2').style.width= '300px';
+    getElementById('div2').style.height= '300px';
+    getElementById('div2').style.background = 'green';
+}
+function toRed() {
+    getElementById('div2').style.width= '200px';
+    getElementById('div2').style.height= '200px';
+    getElementById('div2').style.background = 'red';
+}
