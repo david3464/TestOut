0f71b30a223551f0d7987d27f4b03c92e01ea21c
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-10 19:30:31 -0400

    1

--- 1.On Progress/1.3PA-CircleRotate/app.js
+++ 1.On Progress/1.3PA-CircleRotate/app.js
@@ -11,7 +11,7 @@ var cors = require('cors');
 
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var functionsRouter = require('./routes/functions');
 
 var app = express();
 
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/functions', functionsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- 1.On Progress/1.3PA-CircleRotate/routes/functions.js
+++ 1.On Progress/1.3PA-CircleRotate/routes/functions.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('users/index');
+  res.render('users/index', { layout: 'hero_layout' });
 });
 
 module.exports = router;
