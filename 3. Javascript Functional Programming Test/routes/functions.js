122cde8d4358b0196d568de519bddfee313b4827
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-10 13:30:42 -0400

    1

--- 3. Javascript Functional Programming Test/routes/functions.js
+++ 3. Javascript Functional Programming Test/routes/functions.js
@@ -5,7 +5,7 @@ var router = express.Router();
 //descriptions: function index Page
 //comments: set up function list page
 router.get('/', function(req, res, next) {
-  res.render('functions/lecture01');
+  res.render('functions/index');
 });
 
 //router address localhost:2001/functions/01
@@ -13,6 +13,20 @@ router.get('/', function(req, res, next) {
 //comments: 
 router.get('/01', function(req, res, next) {
   res.render('functions/lecture01');
+});
+
+//router address localhost:2001/functions/02
+//descriptions: Lecutre Two Javascript
+//comments: 
+router.get('/02', function(req, res, next) {
+  res.render('functions/lecture02');
+});
+
+//router address localhost:2001/functions/03
+//descriptions: Lecutre Three Javascript
+//comments: 
+router.get('/03', function(req, res, next) {
+  res.render('functions/lecture03');
 });
 
 
