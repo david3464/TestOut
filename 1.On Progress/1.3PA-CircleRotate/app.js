2af33f32d4955a3596af7f54201d060b1e0eda1b
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-10 19:20:36 -0400

    1

--- 1.On Progress/1.3PA-CircleRotate/app.js
+++ 1.On Progress/1.3PA-CircleRotate/app.js
@@ -3,54 +3,37 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var hbs = require('hbs');
-var fs = require('fs');
+var bodyParser = require('body-parser');
+var expressLayouts = require('express-ejs-layouts');
+var cors = require('cors');
+
+
+
 
 var indexRouter = require('./routes/index');
-var functionsRouter = require('./routes/functions');
-var javafunctionRouter = require('./routes/1.0javascriptFunction');
-var photoanimationRouter = require('./routes/1.1photoanimation');
-
+var usersRouter = require('./routes/users');
 
 var app = express();
 
-//view partial dir
-hbs.registerPartial('partial', fs.readFileSync(__dirname + '/views/partial.hbs', 'utf8'));
-hbs.registerPartials(__dirname + '/views/partials');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set('layout', 'layouts/layout');
+
 
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
-app.use('/functions', functionsRouter);
-app.use('/javafunction',javafunctionRouter);
-app.use('/1.1PA/',photoanimationRouter);
-
-//hbs block
-var blocks = {};
-hbs.registerHelper('extend', function(name, context) {
-    var block = blocks[name];
-    if (!block) {
-        block = blocks[name] = [];
-    }
-
-    block.push(context.fn(this)); // for older versions of handlebars, use block.push(context(this));
-});
-
-hbs.registerHelper('block', function(name) {
-    var val = (blocks[name] || []).join('\n');
-
-    // clear the block
-    blocks[name] = [];
-    return val;
-});
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
