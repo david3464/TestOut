5bcbad1f371ac3150c6bfa5aa1c44d7e4badf0f1
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-11 16:17:25 -0400

    1

--- 1.On Progress/2.3CA-Time(Clock)Demo/routes/functions.js
+++ 1.On Progress/2.3CA-Time(Clock)Demo/routes/functions.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('functions/1PhotoAnimation/3CircleRotate', { layout: 'hero_layout' });
+  res.render('functions/2CartoonAnimation/3Time(Clock)Demo', { layout: 'hero_layout' });
 });
 
 module.exports = router;
