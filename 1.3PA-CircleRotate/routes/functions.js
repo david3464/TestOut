bce551c400c0c3de2fd2741f8fad5ac841ec08c7
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-11 19:35:27 -0400

    1

--- 1.3PA-CircleRotate/routes/functions.js
+++ 1.3PA-CircleRotate/routes/functions.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('functions/1PhotoAnimation/3CircleRotate', { layout: 'hero_layout' });
 });
 
+router.get('/decode', function(req, res, next) {
+  res.render('functions/hooh', { layout: 'hero_layout' });
+});
+
+
+
 module.exports = router;
