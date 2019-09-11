aec9f9e9a0d60509e2b7cd4c0f583ae5a32968d5
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-11 11:43:43 -0400

    1

--- 1.On Progress/1.3PA-CircleRotate/routes/functions.js
+++ 1.On Progress/1.3PA-CircleRotate/routes/functions.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('functions/index', { layout: 'hero_layout' });
+  res.render('functions/1PhotoAnimation/3CircleRotate', { layout: 'hero_layout' });
 });
 
 module.exports = router;
