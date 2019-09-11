7f462b4b65b5e994c02f0206e125b7f1e0365796
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-11 19:39:52 -0400

    1

--- 1.On Progress/3.1SpecialEncode/routes/functions.js
+++ 1.On Progress/3.1SpecialEncode/routes/functions.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('functions/1PhotoAnimation/3CircleRotate', { layout: 'hero_layout' });
+  res.render('functions/3SpecialSkill/1SpecialDecode', { layout: 'hero_layout' });
 });
 
 module.exports = router;
