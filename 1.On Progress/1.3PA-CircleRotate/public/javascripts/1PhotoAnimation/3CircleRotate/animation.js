177c9c898f837edec8a87dc1cb035952124c6fe6
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-11 13:27:58 -0400

    1

--- 1.On Progress/1.3PA-CircleRotate/public/javascripts/1PhotoAnimation/3CircleRotate/animation.js
+++ 1.On Progress/1.3PA-CircleRotate/public/javascripts/1PhotoAnimation/3CircleRotate/animation.js
@@ -1,1 +1,13 @@
-
+window.onload = function() { 
+    var oImg = document.getElementsByTagName("img")
+    var deg = 360 / oImg.length
+    // document.getElementsByTagName("img").length 为图片放置个数
+    // 然后我们要遍历到所有图片 简单来说就是访问一个数组或者集合中的元素 访问之后会做一些事情
+    
+    Array.prototype.forEach.call(oImg, function(ele, index, self){
+        ele.style.transform = "rotateY("+deg*index+"deg)";
+    })
+    
+}
+    
+    
