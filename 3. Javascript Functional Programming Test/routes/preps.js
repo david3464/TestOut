3c73729145d5e26016549254282d87bd6c3184a8
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-12 11:51:20 -0400

    1

--- 3. Javascript Functional Programming Test/routes/preps.js
+++ 3. Javascript Functional Programming Test/routes/preps.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-
+//router address localhost:2001/func_preps/01
+//descriptions: Preprequist Course One Javascript
+//comments: 
+router.get('/01', function(req, res, next) {
+    res.render('functions/preps/lecture01');
+  });
 
 
 module.exports = router;
--- 3. Javascript Functional Programming Test/routes/tutorials.js
+++ 3. Javascript Functional Programming Test/routes/tutorials.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-
+//router address localhost:2001/func_tutorials/01
+//descriptions: Tutorials One Javascript
+//comments: 
 
 
 module.exports = router;
