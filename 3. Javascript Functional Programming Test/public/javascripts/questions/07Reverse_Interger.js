7ff3a43882b8b2cfbe57665e7e8d88d2b3a95eef
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-18 12:41:44 -0400

    1

---
+++ 3. Javascript Functional Programming Test/public/javascripts/questions/07Reverse_Interger.js
@@ -0,0 +1,65 @@
+/*
+Specifications 
+    Input 321
+    Output 123
+*/
+// solution 1
+var RI07_1 = function(num) {
+    let y;
+    if(num < 0){
+        y=num.toString().split('').reverse();
+        console.log(y);
+        y.unshift('-');
+        y.pop();
+        y = parseInt(y.join(''));
+        //Check for 32 bit outside of bounds
+        if(y < -2147483648 ) {
+            return 0
+        }
+        //positive case start here
+    } else {
+        y = parseInt(num.toString().split('').reverse().join(''));
+        //Check for 32 bit outside of bounds
+        if (y > 2147483648) {
+            return 0
+        } 
+    }
+    return y
+}
+
+// solution 2 
+var RI07_2 = function (num) {
+    var a = num.toString().split('').reverse().join('');
+    //split is return the first one ps: if negative sign is return NaN
+    return parseInt(a)*Math.sign(num);
+}
+
+// solution 3
+var RI07_3 = function (num) {
+    //create xstring var to change x to string
+    //create resutlt to return at the end (empty string)
+    var xstring = num.toString();
+    var result = '';
+    //if (xstring[first])is '-' 
+        //append '-' to result string
+        //splice first character of string
+    //else if (xstring[last] is 0)
+        //splice last character of string 
+    if (xstring[0] === '-'){
+        result += '-';
+        xstring = xstring.slice(1);
+    } else if (xstring[xstring.length - 1] === '0'){
+        xstring = xstring.slice(0, xstring.length-1)
+    }
+
+    for (var i = xstring.length -1; i >=0; i--){
+        result += xstring[i];
+    }
+    //xstring that is perfectly ready to go through a for loop
+    //reverse for loop throught x string
+    return (parseInt(result));
+};
+
+console.log(RI07_3(1230));
+console.log(RI07_3(-4560));
+console.log(RI07_3(5600));
--- 3. Javascript Functional Programming Test/routes/preps.js
+++ 3. Javascript Functional Programming Test/routes/preps.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+//router address localhost:2001/func_preps/test
+//descriptions: Test for Leetcode Problem
+//comments: 
+router.get('/test', function(req, res, next) {
+  res.render('functions/preps/test');
+});
+
 //router address localhost:2001/func_preps/01
 //descriptions: Preprequist Course One Javascript
 //comments: 
