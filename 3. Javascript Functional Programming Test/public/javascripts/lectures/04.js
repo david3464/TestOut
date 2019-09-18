13cccea154e4d3b4139f684d9240da4e7efff6d6
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-18 15:42:25 -0400

    1

--- 3. Javascript Functional Programming Test/public/javascripts/lectures/04.js
+++ 3. Javascript Functional Programming Test/public/javascripts/lectures/04.js
@@ -1,8 +1,40 @@
 window.onload = function() {
     
-var btn1 = document.getElementById('btn1');
-btn1.onclick = function() {
-    alert('123')
-};
+    var btn1 = document.getElementById('btn1');
+    btn1.onclick = function() {
+        alert('123')
+    };
 
+    var aDiv = document.getElementsByTagName ('div');
+    // aDiv[1].style.background ='red'
+    for (let i=0;i<4;i++){
+        aDiv[i].style.background ='red';
+    };
+
+    var btn2 = document.getElementById('btn2');
+    var oDiv7 = document.getElementById ('div7');
+    var aCh = oDiv7.getElementsByTagName('input');
+    btn2.onclick = function () {
+        for (var i=0;i<aCh.length; i++){
+          aCh[i].checked = true;  
+        }
+    };
+    var btn3 = document.getElementById('btn3');
+    btn3.onclick = function () {
+        for (var i=0;i<aCh.length; i++){
+          aCh[i].checked = false;  
+        }
+    };
+    var btn4 = document.getElementById('btn4');
+    
+    btn4.onclick = function () {
+        for (var i=0;i<aCh.length; i++) {
+            if(aCh[i].checked == true){
+            aCh[i].checked = false;  
+            } else {
+                aCh[i].checked = true;   
+            }
+        }
+    };
 }
+
--- 3. Javascript Functional Programming Test/routes/lectures.js
+++ 3. Javascript Functional Programming Test/routes/lectures.js
@@ -2,32 +2,59 @@ var express = require('express');
 var router = express.Router();
 
 //router address localhost:2001/func_letctures/01
-//descriptions: Lecutre One Javascript
+//descriptions: Lecture One Javascript
 //comments: 
 router.get('/01', function(req, res, next) {
     res.render('functions/lectures/lecture01');
   });
   
   //router address localhost:2001/func_letctures/02
-  //descriptions: Lecutre Two Javascript
+  //descriptions: Lecture Two Javascript
   //comments: 
   router.get('/02', function(req, res, next) {
     res.render('functions/lectures/lecture02');
   });
   
   //router address localhost:2001/func_letctures/03
-  //descriptions: Lecutre Three Javascript
+  //descriptions: Lecture Three Javascript
   //comments: 
   router.get('/03', function(req, res, next) {
     res.render('functions/lectures/lecture03');
   });
   
   //router address localhost:2001/func_letctures/04
-  //descriptions: Lecutre Four Javascript
+  //descriptions: Lecture Four Javascript
   //comments: 
   router.get('/04', function(req, res, next) {
     res.render('functions/lectures/lecture04');
   });
 
+    //router address localhost:2001/func_letctures/05
+  //descriptions: Lecture Five Javascript
+  //comments: 
+  router.get('/05', function(req, res, next) {
+    res.render('functions/lectures/lecture05');
+  });
+
+    //router address localhost:2001/func_letctures/06
+  //descriptions: Lecture Six Javascript
+  //comments: 
+  router.get('/06', function(req, res, next) {
+    res.render('functions/lectures/lecture06');
+  });
+
+    //router address localhost:2001/func_letctures/07
+  //descriptions: Lecture Seven Javascript
+  //comments: 
+  router.get('/07', function(req, res, next) {
+    res.render('functions/lectures/lecture07');
+  });
+
+    //router address localhost:2001/func_letctures/08
+  //descriptions: Lecture Eight Javascript
+  //comments: 
+  router.get('/08', function(req, res, next) {
+    res.render('functions/lectures/lecture08');
+  });
 
 module.exports = router;
