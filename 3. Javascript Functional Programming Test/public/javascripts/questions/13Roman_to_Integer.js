5891fcad8499c059532da381f44f9a6515dd4914
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-23 18:22:37 -0400

    1

--- 3. Javascript Functional Programming Test/public/javascripts/questions/13Roman_to_Integer.js
+++ 3. Javascript Functional Programming Test/public/javascripts/questions/13Roman_to_Integer.js
@@ -1,19 +1,27 @@
 
 var RTI13_1 = function(num) {
   var roman= "";
-  var roman_number = ["M", "CM", "D","CD","C", "XC","L", "XL", "X", "IX", "V", "IV", "I"];
-  var number = ["1000", "900", "500","400","100", "90","50", "40", "10", "9", "5", "4", "1"];
-  for (var i = 0; i <number.length; i++){
-    while (num >= number[i]){
-      roman = roman + roman_number[i];
-      num = num -number[i];
-    }
+  var RomanToNumber = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
   }
-
+  
+  return roman;
 };
 
 var RTI13_2 = function(num) {
-  var RomanToNum = {
+  var romantonum = {
     M: 1000,
     CM: 900,
     D: 500,
@@ -44,7 +52,21 @@ var RTI13_2 = function(num) {
   return roman;
 };
 
+var RTI13_3 = function(num) {
+  var roman= "";
+  var roman_number = ["M", "CM", "D","CD","C", "XC","L", "XL", "X", "IX", "V", "IV", "I"];
+  var number = ["1000", "900", "500","400","100", "90","50", "40", "10", "9", "5", "4", "1"];
+  for (var i = 0; i <number.length; i++){
+    while (num >= number[i]){
+      roman += roman_number[i];
+      num -= number[i];
+      console.log(roman);
+      console.log(num);
+    }
+  }
+  return roman;
+};
 
 
-console.log(RTI13_1(36));
+console.log(RTI13_1(3001));
 // console.log(RTI13_2(3876));
