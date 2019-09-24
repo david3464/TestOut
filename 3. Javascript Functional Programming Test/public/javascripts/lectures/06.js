94f809028b4e56074301cefbf7ee65ab0bac508e
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-24 17:40:53 -0400

    1

--- 3. Javascript Functional Programming Test/public/javascripts/lectures/06.js
+++ 3. Javascript Functional Programming Test/public/javascripts/lectures/06.js
@@ -1,9 +1,27 @@
-window onload = function() {
-  var oTx1 = document.getElementById('tx1');
-  var oTx2 = document.getElementById('tx2');
+window.onload = function() {
+  var oTx1 = document.getElementById('txt1');
+  var oTx2 = document.getElementById('txt2');
   var oBtn = document.getElementById('btn1');
 
   oBtn.onclick = function() {
-    alert (oTx1.value + oTx2.value)
+    console.log(oTx1.value);
+    console.log(oTx2.value);
+    alert(oTx1.value + oTx2.value);
   };
+
+  var aLi = document.getElementsByTagName('li')
+  for (var i = 0; i <aLi.length; i++){
+    if (i%2 == 0) {
+      aLi[i].style.background = "#CCC";
+    } else {
+      aLi[i].style.background = "";
+    }
+  }
+
+  // for(var i = 0; i <5; i++){
+  //   if(i ===2){
+  //     continue;
+  //   }
+  //   alert(i)
+  // }
 }
---
+++ 3. Javascript Functional Programming Test/public/javascripts/lectures/07.js
@@ -0,0 +1,39 @@
+function sum () {
+  // alert(arguments.length);
+  result = 0; 
+  for (var i = 0;i<arguments.length;i++) {
+    result += arguments[i];
+  };
+  return result;
+};
+
+function css(obj, name, value) {
+  if(arguments.length == 2){
+    return ojb.style[name];
+  } else {
+    obj.style[name] = value;
+
+  }
+}
+
+function getStyle(obj, name) {
+  if(obj.currentStyle) {
+    return obj.currentStyle[name];
+  } else {
+    return getComputedStyle(obj,false)[name];
+  }
+}
+
+var arr = [1,3,6,2,4,7,0,3,56,7,4,6,4,6]
+arr.sort(function(n1, n2){
+  return n1-n2
+})
+console.log(arr);
+
+
+
+
+window.onload = function() {
+  var oDiv1 = document.getElementById('div1');
+  console.log(getStyle(oDiv1, 'width'));
+}
