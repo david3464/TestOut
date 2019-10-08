ce24417554d2f8696a1ce1d48e80238b0265c563
david3464 <51375293+david3464@users.noreply.github.com>
2019-10-08 17:22:24 -0400

    1

--- 4. React Learning/hello-world/src/App.js
+++ 4. React Learning/hello-world/src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+// import Hello from './components/Hello';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet name="Bruce" heroName="Batman"/>
+        <p>This is Children Props</p>
+      <Greet />
+      <Greet name="Andy" heroName="Superman"/>
+      <Greet name="Clavin" heroName="Wonder WOmen"/>
+      <Welcome />
+      {/* <Hello /> */}
     </div>
   );
 }
