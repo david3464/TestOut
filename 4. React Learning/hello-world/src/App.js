4517b618fc68ceeed7998e7541745472e7a42e3a
david3464 <51375293+david3464@users.noreply.github.com>
2019-10-09 13:09:44 -0400

    1

--- 4. React Learning/hello-world/src/App.js
+++ 4. React Learning/hello-world/src/App.js
@@ -3,16 +3,18 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 // import Hello from './components/Hello';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Bruce" heroName="Batman"/>
+      <Message />
+      {/* <Greet name="Bruce" heroName="Batman">
         <p>This is Children Props</p>
-      <Greet />
-      <Greet name="Andy" heroName="Superman"/>
-      <Greet name="Clavin" heroName="Wonder WOmen"/>
-      <Welcome />
+      </Greet> */}
+      {/* <Greet name="Andy" heroName="Superman"/> */}
+      {/* <Greet name="Clavin" heroName="Wonder Women"/> */}
+      {/* <Welcome /> */}
       {/* <Hello /> */}
     </div>
   );
