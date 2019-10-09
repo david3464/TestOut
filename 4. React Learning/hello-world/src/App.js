ff640bdd3969105d66037fb2ca3aa4f20ba0a955
david3464 <51375293+david3464@users.noreply.github.com>
2019-10-09 13:39:30 -0400

    1

--- 4. React Learning/hello-world/src/App.js
+++ 4. React Learning/hello-world/src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-// import Hello from './components/Hello';
-import Message from './components/Message';
+import Greet from './Component/Greet';
+import Welcome from './Component/Welcome';
+// import Hello from './Component/Hello';
+// import Message from './Component/Message';
 
-function App() {
-  return (
-    <div className="App">
-      <Message />
-      {/* <Greet name="Bruce" heroName="Batman">
-        <p>This is Children Props</p>
-      </Greet> */}
-      {/* <Greet name="Andy" heroName="Superman"/> */}
-      {/* <Greet name="Clavin" heroName="Wonder Women"/> */}
-      {/* <Welcome /> */}
-      {/* <Hello /> */}
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Greet />
+        {/* <Message /> */}
+        {/* <Greet name="Bruce" heroName="Batman">
+          <p>This is Children Props</p>
+        </Greet> */}
+        {/* <Greet name="Andy" heroName="Superman"/> */}
+        {/* <Greet name="Clavin" heroName="Wonder Women"/> */}
+        <Welcome />
+        {/* <Hello /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
