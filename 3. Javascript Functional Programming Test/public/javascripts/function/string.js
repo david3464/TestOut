f80e52c118d2cd0d4bc2ffdeb6600a35d016290c
david3464 <51375293+david3464@users.noreply.github.com>
2019-10-10 15:12:01 -0400

    1

--- 3. Javascript Functional Programming Test/public/javascripts/function/string.js
+++ 3. Javascript Functional Programming Test/public/javascripts/function/string.js
@@ -1,6 +1,8 @@
-var fruits = ['Apple', 'Banana'];
-
-console.log(fruits.length);
-var first = fruits[0];
-var last = fruits[fruits.length - 1];
+var numbers = [1,2,3,4,5,6];
+console.log(numbers.length);
+var first = numbers[0];
+var last = numbers[numbers.length - 1];
 console.log(first, last);
+numbers.forEach(function(item, index, array) {
+  console.log(item, index);
+});
